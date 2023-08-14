ec2ea90b1a5f5c539d5d75de06985f7c5ce539a0
zehrydogan <zeehraaydogan@gmail.com>
2023-08-14 18:31:10 +0300

    Add project files.

---
+++ src/Acme.BookStore.EntityFrameworkCore/EntityFrameworkCore/IEfCoreAuthorRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Acme.BookStore.Authors
+{
+    public interface IEfCoreAuthorRepository
+    {
+        Task<Author> FindByNameAsync(string name);
+        Task<List<Author>> GetListAsync(int skipCount, int maxResultCount, string sorting, string filter = null);
+    }
+}
