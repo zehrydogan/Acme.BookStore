c0d6841df20c095b51f172731bfd1e7005b4bbe4
oguzyazan-infoline <89526970+oguzyazan-infoline@users.noreply.github.com>
2023-08-23 21:56:25 +0300

    MovieActor FK migration

---
+++ src/Acme.BookStore.EntityFrameworkCore/Migrations/20230823185422_Added_MovieActors_FK.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme.BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedMovieActorsFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AppMovieActors_ActorId",
+                table: "AppMovieActors",
+                column: "ActorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppMovieActors_MovieId",
+                table: "AppMovieActors",
+                column: "MovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppMovieActors_AppActors_ActorId",
+                table: "AppMovieActors",
+                column: "ActorId",
+                principalTable: "AppActors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppMovieActors_AppMovies_MovieId",
+                table: "AppMovieActors",
+                column: "MovieId",
+                principalTable: "AppMovies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppMovieActors_AppActors_ActorId",
+                table: "AppMovieActors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppMovieActors_AppMovies_MovieId",
+                table: "AppMovieActors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppMovieActors_ActorId",
+                table: "AppMovieActors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppMovieActors_MovieId",
+                table: "AppMovieActors");
+        }
+    }
+}
